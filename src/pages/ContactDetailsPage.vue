d4acab39befe53df9f0516f65c8664aa037c3b25
Nik <nn8543@gmail.com>
2024-01-05 12:04:14 +0200

    fixed contacts and sidemenu

--- src/pages/ContactDetailsPage.vue
+++ src/pages/ContactDetailsPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="full-width-page">
-    <q-item class="page-title">Contact Details</q-item>
+    <q-item class="page-title">{{ $t('contactDetails') }}</q-item>
     <q-item class="custom-header">+359 89 0138750</q-item>
     <q-item class="custom-header">nik@nenkov.eu</q-item>
   </q-page>
